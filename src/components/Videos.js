0075f38175681f63dc2753d87fe46ea36e8c9cac
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 02:29:18 +0000

    Render MovieItems over Data

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,19 +1,20 @@
 import MovieItem from "./Movie"
-
+import { data } from "../data"
 const Videos = () => {
     return (
         <div className="bg-[#aaa] grid grid-cols-3 gap-2 min-h-[90vh] p-2">
 
             {/* When finish designing, we will use the api to  get the videos and map over a Movie Item */}
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
+            {
+                data.map((d) => {
+                    return <MovieItem
+                        poster={d.thumbnails[d.thumbnails.length-1].url}
+                        title = {d.metadata.title}
+                        src = {d.assets[d.assets.length-1].url}
+                    />
+                })
+            }
+
         </div>
     )
 }
