f475cb417490a88114121e9715bbe9ca7a741d97
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 01:02:08 +0000

    Create Navbar Component

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return(
+        <div className="w-full bg-gray-800 h-16 flex  justify-around items-center ">
+            <h1 className="text-white font-bold text-xl">Movies<span className="text-blue-900"> App</span></h1>
+            <p className="text-white font-bold">Icon</p>
+        </div>
+    )
+}
+
+export default Navbar
