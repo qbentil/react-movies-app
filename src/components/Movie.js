b922a971c443df6b4bb3a94e875c925e6e288122
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 01:33:23 +0000

    Update single movie compoent

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,15 @@
 const MovieItem = () => {
     return (
         <div className="w-[28rem] h-[18rem] bg-white rounded-lg">
-            <video className="w-full h-[85%] bg-gray-600 rounded-t-lg" poster="https://i.ytimg.com/vi/FbGrkTzx3r8/maxresdefault.jpg" controls />
+            <video 
+                className="w-full h-[85%] bg-black rounded-t-lg" 
+                poster="https://assets1.ignimgs.com/thumbs/userUploaded/2022/4/15/toukenranbuwarriorsfourthteampvtrailerignblogroll-1650056487514_large.jpg" 
+                controls 
+                src="https://assets14.ign.com/videos/zencoder/2022/04/15/1280/0ee90b15b872657a75e0fc2024352f1a-1910000-1650056391.mp4"
+            />
             <div className="pl-2 pt-2">
                 <p className="text-xl font-semibold text-blue-900">Movie Title</p>
+                
             </div>
 
         </div>
