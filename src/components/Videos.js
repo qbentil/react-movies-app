053eee8ebdc67e591ceeb08f826563ab02dfa0df
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 01:04:18 +0000

    API: To do

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,6 +3,8 @@ import MovieItem from "./Movie"
 const Videos = () => {
     return (
         <div className="bg-[#aaa] grid grid-cols-3 gap-2 min-h-[90vh] p-2">
+
+            {/* When finish designing, we will use the api to  get the videos and map over a Movie Item */}
             <MovieItem />
             <MovieItem />
             <MovieItem />
