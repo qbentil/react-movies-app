dbeb2cc7acf1afa29a8d701c1c611222f4344c49
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 01:01:43 +0000

    Set up App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/navbar';
+import Videos from './components/Videos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='m-0 p-0'>
+        <Navbar />
+        <Videos />
     </div>
   );
 }
