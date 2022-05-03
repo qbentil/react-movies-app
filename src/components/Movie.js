545d92906d829b1764372fe3b40e2c40e564d084
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 01:16:54 +0000

    Update MovieItem Compoent

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,11 @@
 const MovieItem = () => {
     return (
         <div className="w-[28rem] h-[18rem] bg-white rounded-lg">
-            <div className="w-full h-[65%] bg-gray-600 rounded-t-lg">
-                
-            </div>
+            <video className="w-full h-[85%] bg-gray-600 rounded-t-lg" poster="https://i.ytimg.com/vi/FbGrkTzx3r8/maxresdefault.jpg" controls />
             <div className="pl-2 pt-2">
                 <p className="text-xl font-semibold text-blue-900">Movie Title</p>
-                <p className="text-lg">Short description</p>
             </div>
-            <div className="ml-[22rem] -mt-[5.8rem] h-14 w-14 rounded-full bg-blue-900 flex justify-center items-center">
-                Icon
-            </div>
+
         </div>
     )
 }
