ad197222c08deaf22aac30dc64dc1c8760cc9e96
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-03 01:33:30 +0000

    Update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,11 @@
+import {BsGithub} from 'react-icons/bs'
 const Navbar = () => {
     return(
         <div className="w-full bg-gray-800 h-16 flex  justify-around items-center ">
             <h1 className="text-white font-bold text-xl">Movies<span className="text-blue-900"> App</span></h1>
-            <p className="text-white font-bold">Icon</p>
+            <div>
+                <BsGithub color='#fff' size={26} />
+            </div>
         </div>
     )
 }
