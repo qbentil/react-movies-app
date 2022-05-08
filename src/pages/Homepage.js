b3ca965169031ddb336f29b71e091a67cb8b23f7
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-08 14:37:36 +0000

    Create default Home Page

---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Videos from '../components/Videos'
+
+function Homepage() {
+  return (
+    <div className='m-0 p-0'>
+        <Videos />
+    </div>
+  )
+}
+
+export default Homepage
