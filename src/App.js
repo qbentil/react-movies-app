3806b6cd416a663eed8f4eb8837b054d4b6a89dd
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-08 16:09:57 +0000

    Remove Static Navbar to Dynamic

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
-import Navbar from './components/Navbar';
 import Watchpage from './pages/Watchpage';
 
 function App() {
   return (
-    
-    <div>
-      <Navbar/>
       <Routes>
         <Route path='/' element = {<Homepage />} />
         <Route path='/watch' element = {<Watchpage />} />
       </Routes>
-    </div>
   );
 }
 
