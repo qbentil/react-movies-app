ab7c127b0986cb3c2581fe889dc48aedc8d9fd16
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-08 20:57:37 +0000

    Add Showcase Hero Section to Hompage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,3 +1,4 @@
+import Herosection from '../components/Herosection'
 import Navbar from '../components/Navbar'
 import React from 'react'
 import Videos from '../components/Videos'
@@ -6,6 +7,7 @@ function Homepage() {
   return (
     <div className='m-0 p-0'>
         <Navbar page={'home'} />
+        <Herosection />
         <Videos />
     </div>
   )
