90140e70935f7d6930b212b151dfaf2bafe1ac28
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-08 16:09:16 +0000

    Handle Navbar Links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,7 @@
 import { BsGithub } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
-const Navbar = () => {
-    const [selected, setSelected] = useState('home')
+const Navbar = ({page}) => {
   return (
     <div className="w-full bg-gray-800 h-16 flex  justify-around items-center ">
       <div className="text-white flex items-center font-bold text-xl">
@@ -15,15 +13,13 @@ const Navbar = () => {
       <div className="flex items-center justify-around">
         <Link
           to="/"
-          className={`mx-2 text-white font-bold pb-1 ${selected === 'home' && '  border-b border-cyan-500'}`}
-          onClick = {() => setSelected('home')}
+          className={`mx-2 text-white font-bold pb-1 ${page === 'home' && '  border-b border-cyan-500'}`}
         >
           Home
         </Link>
         <Link
           to="/watch"
-          className={`mx-2 text-white font-bold pb-1  ${selected === 'watch' && 'border-b border-cyan-500'}`}
-          onClick = {() => setSelected('watch')}
+          className={`mx-2 text-white font-bold pb-1  ${page === 'watch' && 'border-b border-cyan-500'}`}
         >
           Watch
         </Link>
