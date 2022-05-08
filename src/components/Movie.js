66c6869b917c47072b66e344bdc172bb8189e25e
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-08 21:54:22 +0000

    Redesign unit movie item

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,25 @@
+import {BsFillPlayFill} from 'react-icons/bs'
 import { Link } from "react-router-dom";
 
-const MovieItem = ({ poster, src, title }) => {
+const MovieItem = ({ id,poster, src, title }) => {
   return (
-    <div className="w-[28rem] min-h-[18rem] bg-white rounded-lg">
+    <div className="w-[28rem] h-[35rem] bg-white rounded-lg m-2">
+      <Link to={`/watch`} className=''>
+        <div className='relative w-16 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500 z-50 top-56 left-44 items-center flex justify-center  '>
+          <BsFillPlayFill size={40} className={'text-white'} />
+        </div>
+      </Link>
       <video
         className="w-full h-[80%] bg-black rounded-t-lg cursor-pointer"
         poster={poster}
         // controls
         src={src}
       />
-      <Link to='/watch'>
-        <div className="h-[20%] ">
-          <p className="text-xl my-5 mb-10 font-semibold text-blue-900">
+        <div className="h-[30%] p-3">
+          <p className="text-lg pt-1 mb-10 font-semibold text-blue-900">
             {title}
           </p>
         </div>
-      </Link>
     </div>
   );
 };
