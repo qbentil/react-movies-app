982927f2dd261512d4d39b7aab747ddd272ffe89
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-08 16:09:12 +0000

    Handle Navbar Links

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,11 @@
+import Navbar from '../components/Navbar'
 import React from 'react'
 import Videos from '../components/Videos'
 
 function Homepage() {
   return (
     <div className='m-0 p-0'>
+        <Navbar page={'home'} />
         <Videos />
     </div>
   )
