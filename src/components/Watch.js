4a6f2b25aca9773ad3179f7bd51585840e2df9de
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-09 11:11:50 +0000

    Implemtn Single Video Watch

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function Watch() {
+// import { data } from "../data"
+
+function Watch({movie}) {
   return (
-    <div>Watch</div>
-  )
+      <div className="w-[63.5%] h-[38rem] bg-white rounded-lg m-2 overflow-y-auto">
+        <video
+          className="w-full h-[80%] bg-transparent rounded-t-lg cursor-pointer"
+          poster={movie.thumbnails[2].url}
+          controls
+          src={movie.assets[2].url}
+          autoPlay
+        />
+        <div className="h-[30%] p-3">
+          <p className="text-xl pt-1 font-bold border-b border-cyan-500 text-blue-900">
+            {movie.metadata.title}
+          </p>
+          <p>
+            {movie.metadata.description}
+          </p>
+        </div>
+      </div>
+  );
 }
 
-export default Watch
+export default Watch;
