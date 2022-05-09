9c2b6afa29c7833e1fbcc3e85dbd7c3c7acfc675
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-09 11:11:23 +0000

    Update Movie COmponent

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import {BsFillPlayFill} from 'react-icons/bs'
 import { Link } from "react-router-dom";
 
-const MovieItem = ({ id,poster, src, title }) => {
+const MovieItem = ({video}) => {
+  const poster=video.thumbnails[video.thumbnails.length-1].url
+  const title = video.metadata.title
+  const src = video.assets[video.assets.length-1].url
   return (
     <div className="w-[28rem] h-[35rem] bg-white rounded-lg m-2">
-      <Link to={`/watch`} className=''>
+      <Link to={`/watch/:${video.metadata.slug}`} state = {{movie: video}} className=''>
         <div className='relative w-16 rounded-xl bg-cyan-500 shadow-lg shadow-cyan-500 z-50 top-56 left-44 items-center flex justify-center  '>
           <BsFillPlayFill size={40} className={'text-white'} />
         </div>
